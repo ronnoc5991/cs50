88b9d32dcc81c896d9af8a81fd30a31baa8ed09e
Connor Streng <connorstreng@Connors-MacBook-Pro.local>
2023-01-06 16:30:44 -0600

    Remove completed TODOs from inheritance.c

--- labs/week_5/inheritance.c
+++ labs/week_5/inheritance.c
@@ -39,7 +39,6 @@ int main(void)
 // Create a new individual with `generations`
 person *create_family(int generations)
 {
-    // TODO: Allocate memory for new person
     person *new_person = malloc(sizeof(person));
 
     // If there are still generations left to create
@@ -49,11 +48,9 @@ person *create_family(int generations)
         person *parent0 = create_family(generations - 1);
         person *parent1 = create_family(generations - 1);
 
-        // TODO: Set parent pointers for current person
         new_person->parents[0] = parent0;
         new_person->parents[1] = parent1;
 
-        // TODO: Randomly assign current person's alleles based on the alleles of their parents
         new_person->alleles[0] = parent0->alleles[rand() % 2];
         new_person->alleles[1] = parent1->alleles[rand() % 2];
     }
@@ -61,32 +58,26 @@ person *create_family(int generations)
     // If there are no generations left to create
     else
     {
-        // TODO: Set parent pointers to NULL
         new_person->parents[0] = NULL;
         new_person->parents[1] = NULL;
 
-        // TODO: Randomly assign alleles
         new_person->alleles[0] = random_allele(); 
         new_person->alleles[1] = random_allele(); 
 
     }
 
-    // TODO: Return newly created person
     return new_person;
 }
 
 // Free `p` and all ancestors of `p`.
 void free_family(person *p)
 {
-    // TODO: Handle base case
     if (p == NULL)
         return;
 
-    // TODO: Free parents recursively
     free_family(p->parents[0]);
     free_family(p->parents[1]);
 
-    // TODO: Free child
     printf("Child freed\n");
     free(p);
 }
@@ -94,7 +85,6 @@ void free_family(person *p)
 // Print each family member and their alleles.
 void print_family(person *p, int generation)
 {
-    // Handle base case
     if (p == NULL)
     {
         return;
