55c23a5666b234bd94acc73c24186cb526efb78c
Connor Streng <connorstreng@Connors-MacBook-Pro.local>
2023-01-06 16:30:16 -0600

    DRY cash.c up with use of for loop

--- problem_sets/week_1/cash/cash.c
+++ problem_sets/week_1/cash/cash.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
-const int VALUE_OF_QUARTER = 25;
-const int VALUE_OF_DIME = 10;
-const int VALUE_OF_NICKEL = 5;
-const int VALUE_OF_PENNY = 1;
+const int NUMBER_OF_COIN_VARIANTS = 4;
+const int COIN_VALUES[] = { 25, 10, 5, 1 };
 
 int get_number_of_coins(int cents, int value_of_coin);
 
 int main(void)
 {
-    int cents = 0;
+    int cents;
     int number_of_coins = 0;
 
     do
@@ -18,21 +16,12 @@ int main(void)
         scanf("%d", &cents);
     } while (cents < 0);
 
-    int number_of_quarters = get_number_of_coins(cents, VALUE_OF_QUARTER);
-    cents -= number_of_quarters * VALUE_OF_QUARTER;
-    number_of_coins += number_of_quarters;
-
-    int number_of_dimes = get_number_of_coins(cents, VALUE_OF_DIME);
-    cents -= number_of_dimes * VALUE_OF_DIME;
-    number_of_coins += number_of_dimes;
-
-    int number_of_nickels = get_number_of_coins(cents, VALUE_OF_NICKEL);
-    cents -= number_of_nickels * VALUE_OF_NICKEL;
-    number_of_coins += number_of_nickels;
-
-    int number_of_pennies = get_number_of_coins(cents, VALUE_OF_PENNY);
-    cents -= number_of_pennies * VALUE_OF_PENNY;
-    number_of_coins += number_of_pennies;
+    for (int i = 0; i < NUMBER_OF_COIN_VARIANTS; i ++)
+    {
+        int coin_variant_count = get_number_of_coins(cents, COIN_VALUES[i]);
+        cents -= coin_variant_count * COIN_VALUES[i];
+        number_of_coins += coin_variant_count;
+    }
 
     printf("Number of Coins: %d\n", number_of_coins);
 }
